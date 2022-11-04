ee35cc21c8d25df99d1d3fe6887a9a1636bbcc44
Lucas Teixeira Vieira <lucas.teixeira34@gmail.com>
2022-11-04 16:08:12 -0300

    implementando listagem de bolões via API (mobile).

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,43 @@
-import { VStack, Icon } from "native-base";
+import { useState, useEffect } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "../components/Header";
 import { Button } from "../components/Button";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+
+import { api } from "../api/api";
 
 export function Pools() {
+  const toast = useToast();
   const { navigate } = useNavigation();
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (err) {
+      console.log(err);
+      toast.show({
+        title: "Não foi possível carregar os bolões.",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -18,6 +49,20 @@ export function Pools() {
           onPress={() => navigate("find")}
         />
       </VStack>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          px={5}
+        />
+      )}
     </VStack>
   );
 }
