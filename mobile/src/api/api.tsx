9b80f8ce6baf64e2b96a261a7e0eb91f3122fba4
Lucas Teixeira Vieira <lucas.teixeira34@gmail.com>
2022-11-05 02:45:22 -0300

    criando arquivo EAS para deploy no expo.

--- mobile/src/api/api.tsx
+++ mobile/src/api/api.tsx
@@ -3,5 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   // NO IOS O LOCAHOST FUNCIONA, NO ANDROID NÃO
   // UTILIZAR O IP FÍSICO DA MÁQUINA
-  baseURL: "http://192.168.1.10:3333",
+  baseURL: process.env.API_URL,
 });
